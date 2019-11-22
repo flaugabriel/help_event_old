a27212c249c80b2b26487104ff3e8cabe35fd1aa
Gabriel Flauzino <gabrielflauzino@sefin.ro.gov.br>
2019-11-22 08:58:10 -0400

    removi controler e refiz

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,67 @@
+class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
+  # GET /items
+  # GET /items.json
+  def index
+    @items = Item.all
+  end
+
+  # GET /items/1
+  # GET /items/1.json
+  def show
+  end
+
+  # GET /items/new
+  def new
+    @item = Item.new
+  end
+
+  # GET /items/1/edit
+  def edit
+  end
+
+  # POST /items
+  # POST /items.json
+  def create
+    @item = Item.new(item_params.merge(user_id: current_user.id))
+    if @item.save
+      flash[:success] = 'Item criado!'
+      redirect_to items_path
+    else
+      flash[:error] = @item.errors.full_messagers.to_sentence
+      redirect_to items_path
+    end
+  end
+
+  # PATCH/PUT /items/1
+  # PATCH/PUT /items/1.json
+  def update
+    if @item.update(item_params)
+      flash[:success] = 'Item atualizado!'
+      redirect_to items_path
+    else
+      flash[:error] = @item.errors.full_messagers.to_sentence
+      redirect_to items_path
+    end
+  end
+
+  # DELETE /items/1
+  # DELETE /items/1.json
+  def destroy
+    @item.destroy
+    respond_to do |format|
+      format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    def item_params
+      params.require(:item).permit(:description, :value, :location, :user_id)
+    end
+end
