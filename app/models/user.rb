4f1b284307057cf5e04e69d03968ef38f78b4ede
Gabriel Flauzino <flaugabriel@gmail.com>
2019-12-07 22:37:38 -0400

    avatar not attached? on show event

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
+	has_one_attached :avatar
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
 				 :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-	has_one_attached :avatar
   validates_uniqueness_of :name
 end
