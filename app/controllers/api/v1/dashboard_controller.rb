b825e1937db6069b9b95298f4159f89be61be5db
Gabriel Flauzino <flaugabriel@gmail.com>
2019-12-09 21:53:25 -0400

    jwt

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -2,6 +2,7 @@
 module Api
   module V1
     class DashboardController < BaseController
+
       def index
         events_all = EventUser.where(user_id: current_user)
         render json: {events_all: events_all}
