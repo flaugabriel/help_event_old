be4f7a475cacadee2733130945910c131ba2ca40
Gabriel Flauzino <flaugabriel@gmail.com>
2019-12-09 21:57:39 -0400

    jwt

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class BaseController < ApplicationController
+  protect_from_forgery with: :null_session
+
   include DeviseTokenAuth::Concerns::SetUserByToken
 
 end
