0d75252f39e4dce1aec36dba46b90220166ba7b7
gabriel <flaugabriel@gmail.com>
2019-12-30 10:37:57 -0400

    vai

--- app/controllers/api/v1/event_items_controller.rb
+++ app/controllers/api/v1/event_items_controller.rb
@@ -2,6 +2,7 @@
 module Api
   module V1
     class EventItemsController < BaseController
+      skip_before_action :verify_authenticity_token
       include DeviseTokenAuth::Concerns::SetUserByToken
 
       before_action :authenticate_user!
